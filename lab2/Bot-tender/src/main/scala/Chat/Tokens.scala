3566b45d4a8f75ce3e3ada6d2f81f6d7c3788fbb
SashaKor <alexandra.korukova@gmail.com>
2019-04-01 23:43:39 +0200

    first part done

--- lab2/Bot-tender/src/main/scala/Chat/Tokens.scala
+++ lab2/Bot-tender/src/main/scala/Chat/Tokens.scala
@@ -15,9 +15,31 @@ object Tokens {
   // Products
   val BIERE: Token       = 6
   val CROISSANT: Token   = 7
+  val CHIPS: Token       = 15
+  // Brands
+  val MAISON: Token      = 16
+  val CAILLER: Token     = 17
+  val FARMER: Token      = 18
+  val BOXER: Token       = 19
+  val WITTEKOP: Token    = 20
+  val PUNKIPA: Token     = 21
+  val JACKHAMMER: Token  = 22
+  val TENEBREUSE: Token  = 23
   // Utils
   val PSEUDO: Token      = 9
   val NUM: Token         = 10
+  val COMMANDER: Token   = 24
+  val CONNAITRE: Token   = 25
+  val MON: Token         = 26
+  val SOLDE: Token       = 27
+  val COMBIEN: Token     = 28
+  val COUTE: Token       = 29
+  val COUTENT: Token     = 29
+  val QUEL: Token        = 30
+  val LE: Token          = 31
+  val PRIX: Token        = 32
+  val DE: Token          = 33
+
   val UNKNOWN: Token     = 11
   val EOL: Token         = 12
   // Test
